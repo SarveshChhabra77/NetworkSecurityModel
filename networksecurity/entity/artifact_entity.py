a64b0aba67851e3f3cfce11183a864b0fc6f8c0d
Sarvesh Chhabra <sarveshpoker@gmail.com>
2025-07-27 19:36:06 +0530

    Data ingestion component completed

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtificats:
+    trained_file_path:str
+    tested_file_path:str
--- push_data.py
+++ push_data.py
@@ -4,7 +4,6 @@ import json
 from dotenv import load_dotenv
 import certifi
 import pandas as pd
-import numpy as np
 import pymongo
 from networksecurity.exceptions.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
